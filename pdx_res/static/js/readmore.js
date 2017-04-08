4859787d635628d8be114d62771335277bf7d786
Dan Wright <muscle.crab.13@gmail.com>
2017-04-08 13:46:38 -0700

    responsiveness tweaks

---
+++ pdx_res/static/js/readmore.js
@@ -0,0 +1,6 @@
+// $('.readmore').click(function(){
+// 	console.log(this);
+//     // $('.read').toggleClass('read-less');
+//     // if($(this).text()=='Show Less') $(this).text('Show More'); 
+//     // else  $(this).text('Show Less'); 
+// });
