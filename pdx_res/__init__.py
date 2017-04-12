0d18032066a3dc351630bbab3be52cd9f7861748
Dan Wright <muscle.crab.13@gmail.com>
2017-04-12 15:49:01 -0700

    work out basic medium requesting

--- pdx_res/__init__.py
+++ pdx_res/__init__.py
@@ -1,6 +1,26 @@
 from datetime import datetime
 from flask import Flask, render_template, request, g, abort, jsonify, url_for
 import pytumblr, os
+import requests, json, xmltodict
+
+def medium_get():
+	url = 'https://medium.com/_/api/users/113dce49fa97/profile/stream'
+	headers = {'Accept': 'application/json'}
+	r = requests.get(url, headers=headers)
+	clean_response = r.text.replace('])}while(1);', '').replace('</x>', '')
+	return clean_response
+
+def medium_post_get():
+	url = 'https://medium.com/_/api/posts/a1aeb9667525'
+	r = requests.get(url)
+	return r.text
+
+def medium_xml_get():
+	url = 'https://medium.com/feed/@pdxresistance'
+	headers = {'Accept': 'application/json'}
+	r = requests.get(url, headers=headers)
+	clean_response = r.text.replace('])}while(1);', '').replace('</x>', '')
+	return clean_response
 
 
 app = Flask(__name__)
@@ -18,6 +38,44 @@ def home():
 def about():
 	return render_template('page/about.html')
 
+@app.route('/posts')
+def posts():
+	cooked_json = json.loads(medium_get())
+	all_posts = cooked_json['payload']['references']['Post']
+	return jsonify(all_posts)
+
+@app.route('/a_post')
+def a_post():
+	cooked_json = json.loads(medium_post_get())
+	return jsonify(cooked_json)
+
+@app.route('/xml_post')
+def xml_post():
+	data = medium_xml_get()
+	parsedx = xmltodict.parse(data)
+	return jsonify(parsedx)
+
+
+
+@app.route('/post/<post_id>')
+# @app.route('/post')
+def post(post_id):
+	cooked_json = json.loads(medium_get())
+	all_posts = cooked_json['payload']['references']['Post']
+
+	# print(dir(all_posts))
+	post_keys = all_posts.keys()
+	chosen_key = post_keys[int(post_id)]
+	chosen_post = all_posts[chosen_key]
+	post_title = chosen_post['title']
+	post_subtitle = chosen_post['virtuals']['subtitle']
+	post_meta = chosen_post['virtuals']['metaDescription']
+
+	digested = {'title': post_title, 'subtitle': post_subtitle, 'meta': post_meta }
+
+	# https://medium.com/_/api/posts/4114cd73ba5c
+	# will get us the post, with id 4114cd73ba5c.
+	return jsonify(digested)
 
 
 if __name__ == "__main__":
