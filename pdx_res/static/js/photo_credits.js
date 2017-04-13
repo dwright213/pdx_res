27e79359c3108ec5a548987cf4db8e2f8e1d736a
Dan Wright <muscle.crab.13@gmail.com>
2017-04-13 15:45:40 -0700

    jquery our background photo credit in there

--- pdx_res/static/js/photo_credits.js
+++ pdx_res/static/js/photo_credits.js
@@ -1,22 +1,7 @@
 
 var
-	img1 = $('#main>div').css('background-image'),
-	photog = '';
+	imageName = $('#main>div').css('background-image'),
+	photog = imageName.split('-')[1];
 
-	photog = img1.split('-');
-
-console.log(photog[1]);
-
-
-// css
-//     color: rgba(255, 255, 255, 0.5);
-//     font-weight: 600;
-//     position: fixed;
-//     /* background-color: red; */
-//     top: 71%;
-//     right: 2px;
-//     font-size: 2rem;
-//     transform: rotate(90deg);
-//     width: 200px;
-//     height: 50px;
-//     white-space: nowrap;
+console.log(photog);
+$('div#photocredit>div').attr('data-content', photog)
